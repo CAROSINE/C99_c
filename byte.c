fa467544b58b0aaf8f6738a638dad5672d04441c
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2025-01-19 19:59:56 +0600

    Create byte.c

---
+++ byte.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+    printf("size of int =%d butes \n",sizeof(i));
+    printf("size of float =%d butes \n",sizeof(f));
+    printf("size of double =%d butes \n",sizeof(d));
+    printf("size of char =%d butes \n",sizeof(c));
+}
