476dcd1e410de857e526d66255817668a57c7b48
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2025-02-07 21:01:57 +0600

    Add files via upload

---
+++ getchar.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char character;
+  printf("enter any character:");
+  character=getchar();
+  printf("You entered:%c\n", character);
+}
---
+++ putchar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char character;
+  printf("enter any character:");
+  character=getchar();
+  printf("You entered:");
+  putchar(character);
+}
