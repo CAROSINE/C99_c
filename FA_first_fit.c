eb989e2628ca318792da2cab14f3a4f8d91818d8
CAROSINE <moshiur287rahman@gmail.com>
2026-05-08 22:26:29 +0600

    ^_^

--- FA_first_fit.c
+++ FA_first_fit.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int n;
     printf("Enter number of memory blocks: ");
     scanf("%d", &n);
 
     int memory[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         printf("Enter size of block %d: ", i + 1);
         scanf("%d", &memory[i]);
     }
 
-    while (1) {
+    while (1)
+    {
         int size, allocated = 0;
         printf("\nEnter size needed (0 to exit): ");
         scanf("%d", &size);
 
         if (size == 0) break;
 
-        for (int i = 0; i < n; i++) {
-            if (memory[i] >= size) {
-                memory[i] -= size;
+        for (int i = 0; i < n; i++) 
+        {
+            if (memory[i] >= size) 
+            {
                 printf("Allocated in block %d\n", i + 1);
                 allocated = 1;
                 break;
