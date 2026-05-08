3b35b2e1bdc48ead4024829bd4fd92416665b7da
CAROSINE <moshiur287rahman@gmail.com>
2026-05-08 22:23:22 +0600

    ^_^

--- FA_next_fit.c
+++ FA_next_fit.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int n;
     printf("Enter number of memory blocks: ");
     scanf("%d", &n);
 
     int memory[n];
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++) 
+    {
         printf("Enter size of block %d: ", i + 1);
         scanf("%d", &memory[i]);
     }
 
     int lastIndex = 0; // Keep track of where we last allocated
 
-    while (1) {
+    while (1) 
+    {
         int size, allocated = 0;
         printf("\nEnter size needed (0 to exit): ");
         scanf("%d", &size);
@@ -21,9 +25,11 @@ int main() {
         if (size == 0) break;
 
         // Start searching from lastIndex
-        for (int count = 0; count < n; count++) {
+        for (int count = 0; count < n; count++) 
+        {
             int i = (lastIndex + count) % n; // Wrap around
-            if (memory[i] >= size) {
+            if (memory[i] >= size) 
+            {
                 memory[i] -= size;
                 printf("Allocated in block %d\n", i + 1);
                 lastIndex = i; // Update lastIndex to current block
