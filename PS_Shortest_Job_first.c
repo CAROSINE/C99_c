2c5e8c994e77941aa6b3fc40f241ae0b17c118f6
CAROSINE <moshiur287rahman@gmail.com>
2026-05-10 21:16:58 +0600

    SJF

--- PS_Shortest_Job_first.c
+++ PS_Shortest_Job_first.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int n;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
@@ -9,7 +10,8 @@ int main() {
     int i, j, temp, pos, current_time = 0;
 
     // Input process details
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         process[i] = i + 1;
         printf("Enter arrival time of process %d: ", process[i]);
         scanf("%d", &arrival[i]);
@@ -19,10 +21,13 @@ int main() {
     }
 
     // Sorting processes based on arrival time (initial order)
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         pos = i;
-        for (j = i + 1; j < n; j++) {
-            if (arrival[j] < arrival[pos]) {
+        for (j = i + 1; j < n; j++)
+         {
+            if (arrival[j] < arrival[pos]) 
+            {
                 pos = j;
             }
         }
@@ -36,36 +41,45 @@ int main() {
     int completed = 0, is_completed[n];
     for (i = 0; i < n; i++) is_completed[i] = 0;
 
-    while (completed != n) {
+    while (completed != n)
+     {
         int min_burst = 9999, index = -1;
-        for (i = 0; i < n; i++) {
-            if (arrival[i] <= current_time && is_completed[i] == 0) {
-                if (burst[i] < min_burst) {
+        for (i = 0; i < n; i++) 
+        {
+            if (arrival[i] <= current_time && is_completed[i] == 0) 
+            {
+                if (burst[i] < min_burst)
+                {
                     min_burst = burst[i];
                     index = i;
                 }
                 // If burst times are equal, pick the one that arrived first
-                else if (burst[i] == min_burst && arrival[i] < arrival[index]) {
+                else if (burst[i] == min_burst && arrival[i] < arrival[index]) 
+                {
                     index = i;
                 }
             }
         }
 
-        if (index != -1) {
+        if (index != -1) 
+        {
             completion[index] = current_time + burst[index];
             turnaround[index] = completion[index] - arrival[index];
             waiting[index] = turnaround[index] - burst[index];
             current_time = completion[index];
             is_completed[index] = 1;
             completed++;
-        } else {
+        } 
+        else 
+        {
             current_time++;
         }
     }
 
     // Printing the final process table
     printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",
                process[i], arrival[i], burst[i], completion[i], turnaround[i], waiting[i]);
     }
