ba54683afba492f6ac02beac2d991190f8dede52
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-05-31 10:13:46 +0600

    Update 1,2,4,8,16.c

--- 1,2,4,8,16.c
+++ 1,2,4,8,16.c
@@ -4,9 +4,10 @@ int main ()
     int n,i;
     printf("ENTER ANY NUMBER :");
     scanf("%d",&n);
+    
     for(int i=1;i<=n;i*=2)
     {
-    printf("%d\n",i);
+       printf("%d\n",i);
     }
     return 0 ;
 }
