9eafe9eee6b00c5989a6d760aae935a500e8b343
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-05-31 10:17:51 +0600

    Update 1009.c

--- 1009.c
+++ 1009.c
@@ -4,9 +4,11 @@ int main()
 {
     char name[50];
     double fixedsalary, sales, total;
+    
     scanf("%s", name);
     scanf("%lf", &fixedsalary);
     scanf("%lf", &sales);
+    
     total = fixedsalary + (sales * 0.15);
     printf("TOTAL = R$ %.2lf\n", total);
     return 0;
