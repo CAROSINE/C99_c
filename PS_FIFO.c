e111d11665d02e6c05b3609d2ba5a6a55808cfc0
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-06-10 23:26:15 +0600

    Update PS_FIFO.c

--- PS_FIFO.c
+++ PS_FIFO.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int n;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
@@ -19,10 +20,13 @@ int main() {
     }
 
     // Sorting processes based on arrival time (FCFS scheduling)
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         pos = i;
-        for (j = i + 1; j < n; j++) {
-            if (arrival[pos] > arrival[j]) {
+        for (j = i + 1; j < n; j++) 
+        {
+            if (arrival[pos] > arrival[j]) 
+            {
                 pos = j;
             }
         }
@@ -44,13 +48,19 @@ int main() {
     }
 
     // Calculating Completion Time
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         if (i == 0) {
             completion[i] = arrival[i] + execution[i];
-        } else {
-            if (completion[i - 1] > arrival[i]) {
+        } 
+        else 
+        {
+            if (completion[i - 1] > arrival[i])
+            {
                 completion[i] = completion[i - 1] + execution[i];
-            } else {
+            } 
+            else 
+            {
                 completion[i] = arrival[i] + execution[i];
             }
         }
@@ -58,7 +68,8 @@ int main() {
 
     // Printing final table after calculating completion times
     printf("\nProcess\tArrival Time\tExecution Time\tCompletion Time\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         printf("%d\t%d\t\t%d\t\t%d\n", process[i], arrival[i], execution[i], completion[i]);
     }
 
